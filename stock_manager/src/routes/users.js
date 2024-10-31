f254290e53818895e00d364f8b55f63936353f38
Evelin Montalvo Martinez <evelinmontalvomtz@gmail.com>
2024-10-31 11:52:35 -0600

    31/10/2024

--- stock_manager/src/routes/users.js
+++ stock_manager/src/routes/users.js
@@ -1,8 +1,11 @@
-const {Router} = require('express');
-const {getMessage} = require('../controllers/users');
+const { Router } = require('express');
+const { getAll, getById, createUser, updateUser } = require('../controllers/users');
 
 const router = Router();
 
-router.get('/', getMessage);
+router.get('/', getAll);
+router.get('/:id', getById);
+router.post('/', createUser);
+router.put('/:id', updateUser)
 
 module.exports = router;
