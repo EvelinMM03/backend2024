814a64b9fa5c5b2f5ad9e616939cdf0e2fc8e099
Evelin Montalvo Martinez <evelinmontalvomtz@gmail.com>
2024-12-09 11:27:37 -0600

    09/12/2024

--- TareaEquipo10/src/routes/clients.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllClients, createClient } = require("../controllers/clientsController");
-
-router.get("/", getAllClients);
-router.post("/", createClient);
-
-module.exports = router;
--- TareaEquipo10/src/routes/sales.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllSales, createSale } = require("../controllers/salesController");
-
-router.get("/", getAllSales);
-router.post("/", createSale);
-
-module.exports = router;
